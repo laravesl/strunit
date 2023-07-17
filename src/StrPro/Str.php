602d3f92eda442114771f42924cd06ecd4e2ecc3
subhashwebiots <envatoverify@webiots.com>
2023-07-17 16:13:05 +0530

    change endpoints

--- src/StrPro/Str.php
+++ src/StrPro/Str.php
@@ -15,7 +15,7 @@ class Str
             if (file_exists($fP)) {
                 $jD = file_get_contents($fP);
                 if ($jD && isset($jD)) {
-                    return Http::post(dbString('aHR0cHM6Ly9saWNlbnNlLndlYmlvdHMuY28uaW4vYXBpL3Jlc2V0L2xpY2Vuc2U='),[
+                    return Http::post(dbString('aHR0cHM6Ly9sYXJhdmVsLnBpeGVsc3RyYXAubmV0L3ZlcmlmeS9hcGkvcmVzZXQvbGljZW5zZQ=='),[
                         dbString('a2V5') => dbString($jD)
                     ]);
                 }
@@ -33,7 +33,7 @@ class Str
 
             $ls = $r->all();
             if (strPrp()) {
-                return Http::post(dbString('aHR0cHM6Ly9saWNlbnNlLndlYmlvdHMuY28uaW4vYXBpL2VudmF0bw=='),[
+                return Http::post(dbString('aHR0cHM6Ly9sYXJhdmVsLnBpeGVsc3RyYXAubmV0L3ZlcmlmeS9hcGkvZW52YXRv'),[
                     dbString('a2V5') => trim($ls[dbString('bGljZW5zZQ==')]),
                     dbString('ZW52YXRvX3VzZXJuYW1l') => $ls[dbString('ZW52YXRvX3VzZXJuYW1l')],
                     dbString('ZG9tYWlu') => str_replace(array(dbString('YmxvY2svbGljZW5zZS92ZXJpZnk='), dbString('aW5zdGFsbC9saWNlbnNl'), dbString('aW5zdGFsbC92ZXJpZnk=')), '', url()->current()),
